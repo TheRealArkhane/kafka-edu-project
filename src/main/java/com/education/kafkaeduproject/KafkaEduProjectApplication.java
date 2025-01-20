8ae1187ab8abdb9ff5c681595269b457194b37dc
Victor Fedyanin <fedyanin.v.v@yandex.ru>
2025-01-20 12:19:09 +0300

    Ititial commit

---
+++ src/main/java/com/education/kafkaeduproject/KafkaEduProjectApplication.java
@@ -0,0 +1,13 @@
+package com.education.kafkaeduproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEduProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaEduProjectApplication.class, args);
+    }
+
+}
