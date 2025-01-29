57d862076cf1a71b7c97b36deb6d3a6ed6115ce0
Victor Fedyanin <fedyanin.v.v@yandex.ru>
2025-01-29 16:48:30 +0300

    Kafka: - Add support of 2 profiles: active and united

--- kafka-consumer/src/main/java/com/education/kafkaconsumer/service/KafkaConsumerPostService.java
+++ kafka-consumer/src/main/java/com/education/kafkaconsumer/service/KafkaConsumerPostService.java
@@ -1,17 +1,28 @@
 package com.education.kafkaconsumer.service;
+
 import com.education.kafkaproducer.payload.Post;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@AllArgsConstructor
+@Profile("single")
 public class KafkaConsumerPostService {
 
-    @KafkaListener(topics = "topic_post")
-    public void consume(Post post) {
-        log.info("Message consumed {}", post);
+    @KafkaListener(topics = "topic_post", groupId = "single-group-1")
+    public void consumeSingle1(Post post) {
+        log.info("Single Consumer 1 consumed: {}", post);
+    }
+
+    @KafkaListener(topics = "topic_post", groupId = "single-group-2")
+    public void consumeSingle2(Post post) {
+        log.info("Single Consumer 2 consumed: {}", post);
+    }
+
+    @KafkaListener(topics = "topic_post", groupId = "single-group-3")
+    public void consumeSingle3(Post post) {
+        log.info("Single Consumer 3 consumed: {}", post);
     }
 }
---
+++ kafka-consumer/src/main/java/com/education/kafkaconsumer/service/KafkaConsumerUnitedPostService.java
@@ -0,0 +1,28 @@
+package com.education.kafkaconsumer.service;
+
+import com.education.kafkaproducer.payload.Post;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@Profile("united")
+public class KafkaConsumerUnitedPostService {
+
+    @KafkaListener(topics = "topic_post", groupId = "united-group")
+    public void consumeUnited1(Post post) {
+        log.info("United Consumer 1 consumed: {}", post);
+    }
+
+    @KafkaListener(topics = "topic_post", groupId = "united-group")
+    public void consumeUnited2(Post post) {
+        log.info("United Consumer 2 consumed: {}", post);
+    }
+
+    @KafkaListener(topics = "topic_post", groupId = "united-group")
+    public void consumeUnited3(Post post) {
+        log.info("United Consumer 3 consumed: {}", post);
+    }
+}
